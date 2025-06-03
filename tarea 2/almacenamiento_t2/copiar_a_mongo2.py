da63412189702fa7bc3691515499175f53443e54
Cristobal Zaror Puga <89882424+JamesJustin69@users.noreply.github.com>
2025-06-03 17:34:40 -0400

    Add files via upload

---
+++ tarea 2/almacenamiento_t2/copiar_a_mongo2.py
@@ -0,0 +1,36 @@
+import json
+from pymongo import MongoClient, errors
+
+cliente = MongoClient('mongodb://mongodb_wazeT2:27017/')
+db = cliente['trafico_waze']
+coleccion = db['eventos']
+
+try:
+    coleccion.create_index('id', unique=True)
+    print("Índice único creado en el campo 'id'.")
+except errors.OperationFailure:
+    print("El índice ya existe o ocurrió un error.")
+
+try:
+    with open('eventos_wazeT2.json', 'r', encoding='utf-8') as archivo:
+        datos = json.load(archivo)
+
+    if isinstance(datos, dict):
+        datos = [datos]
+
+except FileNotFoundError:
+    print("No se encontró el archivo 'eventos_wazeT2.json'.")
+    exit(1)
+except json.JSONDecodeError:
+    print("error al leer el .json")
+    exit(1)
+
+insertados = 0
+for evento in datos:
+    try:
+        coleccion.insert_one(evento)
+        insertados += 1
+    except errors.DuplicateKeyError:
+        continue
+
+print(f"Se insertaron {insertados} nuevos eventos en mongoDB.")
